c58d74f209cb1447269272ad8897c566d204aaae
itamar-marom <marom.itamar@gmail.com>
2022-03-04 17:04:27 +0200

    pipeline cmd hirarchy

--- cmd/createPipeline.go
+++ cmd/createPipeline.go
@@ -21,34 +21,32 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// pipelineCmd represents the pipeline command
-var pipelineCmd = &cobra.Command{
-	Use:   "pipeline",
-	Short: "pipeline",
-	Long:  `pipline definition for a specific application in a given repository`,
+// createPipelineCmd represents the createPipeline command
+var createPipelineCmd = &cobra.Command{
+	Use:   "pipeline <name>",
+	Short: "Create a pipeline",
+	Long:  `Create a pipline definition for a specific application and a provider`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("pipeline called")
+		fmt.Println("createPipeline called")
 	},
 }
 
 func init() {
-	getCmd.AddCommand(pipelineCmd)
-	createCmd.AddCommand(pipelineCmd)
-	deleteCmd.AddCommand(pipelineCmd)
+	createPipelineCmd.PersistentFlags().StringP("application", "a", "", "application to be part of")
+	createPipelineCmd.PersistentFlags().StringP("provider", "p", "", "provider to create from")
 
-	pipelineCmd.PersistentFlags().StringP("application", "a", "", "application to be part of")
-	pipelineCmd.PersistentFlags().StringP("api-resource", "r", "", "api-resource to create from")
+	createPipelineCmd.MarkPersistentFlagRequired("application")
+	createPipelineCmd.MarkPersistentFlagRequired("provider")
 
-	pipelineCmd.MarkPersistentFlagRequired("application")
-	pipelineCmd.MarkPersistentFlagRequired("api-resource")
+	createCmd.AddCommand(createPipelineCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// pipelineCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// createPipelineCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// pipelineCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// createPipelineCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
